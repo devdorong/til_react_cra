7349336972395499debbdbac21b79f2a4a817afe
devdorong <dev.dorong@gmail.com>
2025-07-11 15:38:07 +0900

    [docs] 로그인 폼

--- src/components/form/InputUi.jsx
+++ src/components/form/InputUi.jsx
@@ -1,33 +1,10 @@
 import styled from "@emotion/styled";
 import React from "react";
+import { InputGroup, InputStyled, Label } from "./InputUi.styles";
 // 전역자리 (window) : 새로고침 반영 안됨.
 // styled 코드 자리
-const InputGroup = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  margin-bottom: 8px;
-`;
-const Label = styled.label`
-  font-size: 11px;
-  font-weight: 500;
-  color: #333;
-  white-space: nowrap;
-  min-width: 60px;
-`;
-const InputStyled = styled.input`
-  width: 100%;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  padding: 10px;
-  font-size: 12px;
 
-  &:focus {
-    outline: none;
-    border: 1px solid #007bff;
-  }
-`;
-function InputUi({ id, type, value, name, placeholder, label }) {
+function InputUi({ id, type, value, name, placeholder, label, onChange }) {
   //js
 
   //jsx
@@ -40,6 +17,7 @@ function InputUi({ id, type, value, name, placeholder, label }) {
         name={name}
         placeholder={placeholder}
         id={id}
+        onChange={onChange}
       />
     </InputGroup>
   );
